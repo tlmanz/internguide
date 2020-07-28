4e801031cc3283f18c682a1641768a174e4a4717
TharukaMannapperuma <49079409+TharukaMannapperuma@users.noreply.github.com>
2020-07-28 22:31:47 +0530

    Updates

--- admin/config/connect.php
+++ admin/config/connect.php
@@ -1,9 +1,8 @@
 <?php 
-	$connection = mysqli_connect('localhost','root','Tharuka@1243','career');
+	$connection = mysqli_connect('localhost','username','password','db_name');
 	if(!$connection){
 		echo "Error: Unable to connect to MySQL." . PHP_EOL;
 		echo "Debugging errno: " . mysql_connect_errno() . PHP_EOL;
 		echo "Debugging error: " . mysql_connect_error() . PHP_EOL;
 		exit();
 	}
- ?>
